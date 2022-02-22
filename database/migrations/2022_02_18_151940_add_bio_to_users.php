11baa012fc2cb2a165628a11a7b8ac2357e5d768
Krystian Wojciechowski <wojciechowskik1010@gmail.com>
2022-02-22 15:59:59 +0100

    fix

--- database/migrations/2022_02_18_151940_add_bio_to_users.php
+++ database/migrations/2022_02_18_151940_add_bio_to_users.php
@@ -14,7 +14,7 @@ class AddBioToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->text('bio')->default('')->nullable();
+            $table->text('bio')->nullable();
         });
     }
 
--- database/migrations/2022_02_19_194729_add_job_to_users_table.php
+++ database/migrations/2022_02_19_194729_add_job_to_users_table.php
@@ -14,7 +14,7 @@ class AddJobToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->text('job')->default('')->nullable();
+            $table->text('job')->nullable();
         });
     }
 
